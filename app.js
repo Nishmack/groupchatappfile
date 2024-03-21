1f32affe2b2d854e30cefdc42834e65d4f87a260
Nishmack <nishmayuzuf@gmail.com>
2024-03-21 22:56:17 +0530

    sixth commit

--- app.js
+++ app.js
@@ -2,24 +2,15 @@ const express = require("express");
 const bodyparser = require("body-parser");
 
 const app = express();
+const adminRouter = require("./routes/admin");
+const shopRouter = require("./routes/shop");
 
 app.use(bodyparser.urlencoded({ extended: false }));
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title" placeholder="Product Title"><br><input type="number" name="size" placeholder="Product Size"><br><button type="submit">Add product</button></form>'
-  );
-});
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  //console.log(req.body);
 
-  // console.log(req.body.size);
+app.use("/admin", adminRouter);
+app.use(shopRouter);
 
-  res.redirect("/");
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
 });
-app.use("/", (req, res, next) => {
-  console.log("another middleware");
-  res.send("<h1>Hello from Express!</h1>");
-});
-
 app.listen(8000);
