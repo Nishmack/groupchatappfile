b5f4cd9228c70ce07a97d8f83840d00e3d713a25
Nishmack <nishmayuzuf@gmail.com>
2024-03-23 01:25:34 +0530

    seventh commit

--- app.js
+++ app.js
@@ -1,16 +1,52 @@
 const express = require("express");
-const bodyparser = require("body-parser");
+const bodyParser = require("body-parser");
+const fs = require("fs");
 
 const app = express();
-const adminRouter = require("./routes/admin");
-const shopRouter = require("./routes/shop");
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(bodyparser.urlencoded({ extended: false }));
-
-app.use("/admin", adminRouter);
-app.use(shopRouter);
-
-app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+app.get("/", (req, res) => {
+  fs.readFile("username.txt", (err, data) => {
+    if (err) {
+      console.log(err);
+      data = "no chat exist"; // Assign a default value
+    }
+    res.send(
+      `${data}<form action="/" method="POST"><input type="text" name="message" id="message"><input type="hidden" name="username" id="username"><br><button type="submit">Send</button></form><script>document.getElementById('username').value = localStorage.getItem('username');</script>`
+    );
+  });
 });
-app.listen(8000);
+
+app.post("/", (req, res) => {
+  console.log(req.body.username);
+  console.log(req.body.message);
+  fs.writeFileSync(
+    "username.txt",
+    `${req.body.username}:${req.body.message}\n`, // Correct string interpolation
+    { flag: "a" },
+    (err) => {
+      if (err) console.log(err);
+    }
+  );
+  res.redirect("/");
+});
+
+app.get("/login", (req, res) => {
+  res.send(
+    '<form action="/login" method="POST"><input type="text" name="username" id="username"><br><button type="submit">Login</button></form>'
+  );
+});
+
+app.post("/login", (req, res) => {
+  console.log(req.body);
+
+  // Send the username back to the client-side and store it in localStorage using JavaScript
+  const username = req.body.username;
+  res.send(
+    `<script>localStorage.setItem("username", "${username}"); window.location.href = "/";</script>`
+  );
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
